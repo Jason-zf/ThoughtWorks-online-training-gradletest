231a164fdcf05119c70b001304f619a4978deb87
Jason <baofeng_zheng@163.com>
2018-03-30 16:15:58 +0800

    complete section3-atest

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -2,11 +2,16 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class PracticeA {
-    Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
+        List<String> value = object.get("value");
+        Set<String> keys=collectionA.keySet();
+        for (String key : keys) {
+            if(value.contains(key))
+                collectionA.put(key,collectionA.get(key)-1);
+        }
+        return collectionA;
     }
 }
