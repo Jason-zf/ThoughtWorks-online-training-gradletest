b4faab6dcc967cb601dbfc2506e8b771f3da72a4
Jason <baofeng_zheng@163.com>
2018-03-30 10:38:07 +0800

    complete find same elements in two collections

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result=new ArrayList();
+        for (int i = 0; i < collection1.size(); ++i) {
+            String elementA = collection1.get(i);
+            if (collection2.indexOf(elementA) != -1) {
+                result.add(elementA);
+            }
+        }
+        return result;
     }
 }
