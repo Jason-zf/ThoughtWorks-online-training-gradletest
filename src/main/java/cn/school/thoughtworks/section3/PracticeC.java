385f4a62ad47cc5fb79c976da9fc2435f2354cf1
Jason <baofeng_zheng@163.com>
2018-03-30 16:32:24 +0800

    complete section3-ctest

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,22 @@
 package cn.school.thoughtworks.section3;
 
+import cn.school.thoughtworks.section2.PracticeA;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> collection3=new HashMap<>();
+        for(String key:collectionA) {
+            if(collection3.containsKey(key))
+                collection3.put(key,collection3.get(key).intValue()+1);
+            else
+                collection3.put(key,1);
+        }
+        PracticeB practiceB=new PracticeB();
+        collection3=practiceB.createUpdatedCollection(collection3,object);
+        return collection3;
     }
 }
